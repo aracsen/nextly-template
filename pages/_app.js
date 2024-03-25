293d08b77781c817d4efd44f0d2fe04226db3a12
kimsemin <aracsen@snu.ac.kr>
2024-03-25 15:13:32 +0900

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
--- pages/index.js
+++ pages/index.js
@@ -25,35 +25,14 @@ const Home = () => {
       </Head>
 
       <Navbar />
-      <Hero />
-      <SectionTitle
-        pretitle="Nextly Benefits"
-        title=" Why should you use this landing page">
-        Nextly is a free landing page & marketing website template for startups
-        and indie projects. Its built with Next.js & TailwindCSS. And its
-        completely open-source.
-      </SectionTitle>
-      <Benefits data={benefitOne} />
-      <Benefits imgPos="right" data={benefitTwo} />
-      <SectionTitle
-        pretitle="Watch a video"
-        title="Learn how to fullfil your needs">
-        This section is to highlight a promo or demo video of your product.
-        Analysts says a landing page with video has 3% more conversion rate. So,
-        don&apos;t forget to add one. Just like this.
-      </SectionTitle>
-      <Video />
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said">
-        Testimonails is a great way to increase the brand trust and awareness.
-        Use this section to highlight your popular customers.
-      </SectionTitle>
-      <Testimonials />
-      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+      
+      <SectionTitle pretitle="모임하기" title="신청방법">
         Answer your customers possible questions here, it will increase the
         conversion rate as well as support or chat requests.
       </SectionTitle>
+      <form>
+      <textarea className={"flex items-center justify-between w-full max-w-2xl p-2 mx-auto h-64  text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200"}></textarea>
+      </form>
       <Faq />
       <Cta />
       <Footer />
